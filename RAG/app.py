418a3c5610ce868b59ce87916cc0d8810d547396
Uzair <uzairarifjokhio@gmail.com>
2025-12-11 17:55:24 +0500

    Added a moduler Pipeline in src

---
+++ RAG/app.py
@@ -0,0 +1,8 @@
+from src.data_loader import load_all_documents
+from src.vector_store import FaissVectorStore
+
+if __name__ == "__main__":
+    docs = load_all_documents("data")
+    store=FaissVectorStore("faiss_store")
+    store.build_from_documents(docs)
+    
