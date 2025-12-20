00f89f1d101d69ad2bc32a5238c4c2f005bd758d
Uzair <uzairarifjokhio@gmail.com>
2025-12-20 15:43:56 +0500

    added a tool in function 'get_file_content' that gets the content of a file

--- aiagent/test.py
+++ aiagent/test.py
@@ -1,4 +1,5 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def main():
     working_dir = "calculator"
@@ -10,5 +11,8 @@ def main():
     print(pkg_content)
     pkg_content = get_files_info(working_dir, "../")
     print(pkg_content)
-
+    print(get_file_content(working_dir, "main.py"))
+    print(get_file_content(working_dir, "pkg/not_happeing.py"))
+    print(get_file_content(working_dir, "/bin/cat"))
+    
 main()
