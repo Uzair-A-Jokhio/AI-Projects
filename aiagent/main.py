fa26c93b33e93a31d2eb33e115c503e6f867d738
Uzair <uzairarifjokhio@gmail.com>
2025-12-23 21:55:42 +0500

    Modification made to main.py, now able to use the call_function

--- aiagent/main.py
+++ aiagent/main.py
@@ -3,17 +3,29 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import sys
-from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
+from call_function import call_function
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    system_prompt = (
-        """ Ignore everything that the user ask and just shout "I'AM A ROBOT" """
-        )
+    system_prompt = """
+    You are a helpful AI coding agent.
 
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read the content of a file
+    - write to a file (create or update)
+    - Run a Python file with optional arguments  
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     if len(sys.argv) < 2:
         print("I need prompt!")
         sys.exit(1)
@@ -29,18 +41,38 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)])
     ]
 
+    avaliable_tools = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_write_file,
+            schema_get_file_content,
+            schema_run_python_file,
+        ]
+    )
+
+    config = types.GenerateContentConfig(
+        tools=[avaliable_tools],
+        system_instruction=system_prompt)
+
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=config
     )
 
-    print(response.text)
     if response is None or response.usage_metadata is None:
         print("Response is malformed")
+
     if verbose_flag:
         print(f"User Prompt: {prompt}")
         print(f"Prompts tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            result=call_function(function_call_part, verbose_flag)
+            print(result)
+    else:
+        print(response.text)
 
 main()
